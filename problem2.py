8c5e1cf66bb8abd4d6ee9bd074c59eeafcf9318f
clock zhong <zhong.clock@gmail.com>
2017-01-22 15:51:21 +0800

    added problem2.py into project:LearningDeepLearning

---
+++ problem2.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+pickle_file = train_datasets[0]  # index 0 should be all As, 1 = all Bs, etc.
+with open(pickle_file, 'rb') as f:
+    letter_set = pickle.load(f)  # unpickle
+    sample_idx = np.random.randint(len(letter_set))  # pick a random image index
+    sample_image = letter_set[sample_idx, :, :]  # extract a 2D slice
+    plt.figure()
+    plt.imshow(sample_image)  # display it
