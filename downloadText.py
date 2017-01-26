c3954bae55e4490ca27c2e70adf3bfe2a2b68c70
clock zhong <zhong.clock@gmail.com>
2017-01-26 16:27:42 +0800

    added downloadText.py into project:LearningDeepLearning

---
+++ downloadText.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+url = 'http://mattmahoney.net/dc/'
+
+def maybe_download(filename, expected_bytes):
+  """Download a file if not present, and make sure it's the right size."""
+  if not os.path.exists(filename):
+    filename, _ = urlretrieve(url + filename, filename)
+  statinfo = os.stat(filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified %s' % filename)
+  else:
+    print(statinfo.st_size)
+    raise Exception(
+      'Failed to verify ' + filename + '. Can you get to it with a browser?')
+  return filename
+
+filename = maybe_download('text8.zip', 31344016)
+
--- testImport.py
+++ testImport.py
@@ -16,8 +16,20 @@ from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 
+import collections
+import math
+import random
+import zipfile
+from matplotlib import pylab
 from six.moves import range
+from sklearn.manifold import TSNE
 
 # Config the matplotlib backend as plotting inline in IPython
 %matplotlib inline
+
+
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
 
