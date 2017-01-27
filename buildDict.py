0a2d0208b6568f518a657ad3c1ea11c08c0a5cd2
clock zhong <zhong.clock@gmail.com>
2017-01-27 12:52:51 +0800

    added buildDict.py into project:LearningDeepLearning

---
+++ buildDict.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+vocabulary_size = 50000
+
+def build_dataset(words):
+  count = [['UNK', -1]]
+  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+  dictionary = dict()
+  for word, _ in count:
+    dictionary[word] = len(dictionary)
+  data = list()
+  unk_count = 0
+  for word in words:
+    if word in dictionary:
+      index = dictionary[word]
+    else:
+      index = 0  # dictionary['UNK']
+      unk_count = unk_count + 1
+    data.append(index)
+  count[0][1] = unk_count
+  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys())) 
+  return data, count, dictionary, reverse_dictionary
+
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+print('Most common words (+UNK)', count[:5])
+print('Sample data', data[:10])
+del words  # Hint to reduce memory.
